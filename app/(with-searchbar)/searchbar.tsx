e47dde6745c7294e24434e2b65b9941b11aa3b06
donghoonlee <94donghoon.lee@gmail.com>
2024-11-14 17:17:41 +0900

    Server & Client comnent

---
+++ app/(with-searchbar)/searchbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { ChangeEvent, useState } from "react";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <input value={search} onChange={onChangeSearch} />
+      <button>검색</button>
+    </div>
+  );
+}
